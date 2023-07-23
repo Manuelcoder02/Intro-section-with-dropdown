223f41f63c25c2754c5b6236fa104de912593d2f
Sotunde Emmanuel <sotundeemmanuel@gmail.com>
2023-07-23 20:05:49 +0100

    the feature and company toggle on mobile screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,20 @@ const closeNavBtn = document.querySelector(".close-navbar");
 const overlayEL = document.querySelector(".overlay");
 const navigationBarEL = document.querySelector(".navbar-content");
 
+const featToggle = document.querySelector(".feat-text");
+const featureContent = document.querySelector(".feature-content");
+const featArrowUp = document.querySelector(".fu");
+const featArrowDown = document.querySelector(".fd");
+
+const compToggle = document.querySelector(".comp-text");
+const companyContent = document.querySelector(".company-content");
+const compArrowUp = document.querySelector(".cu");
+const compArrowDown = document.querySelector(".cd");
+
+console.log(featArrowDown, featArrowUp);
+// const dropDown = function(){
+
+// }
 openNavBtn.addEventListener("click", function () {
   // when the menu bar is been clicked
   navigationBarEL.classList.add("clicked");
@@ -16,4 +30,18 @@ openNavBtn.addEventListener("click", function () {
 closeNavBtn.addEventListener("click", function () {
   navigationBarEL.classList.remove("clicked");
   overlayEL.classList.add("hidden");
+});
+
+featToggle.addEventListener("click", function () {
+  featureContent.classList.toggle("feature-content-nav");
+  // featArrowDown.style.visibility = "hidden";
+  // featArrowUp.style.visibility = "visible";
+  featArrowDown.classList.toggle("ddd");
+  featArrowUp.classList.toggle("fff");
+});
+
+compToggle.addEventListener("click", function () {
+  companyContent.classList.toggle("company-content-nav");
+  compArrowDown.classList.toggle("ddd");
+  compArrowUp.classList.toggle("fff");
 });
